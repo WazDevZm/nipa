4cbe8095479a001ef6e7127fd090b7bd383afecd
Wazingwa Mugala <wazingwamugala90@gmail.com>
2025-11-04 07:21:00 +0200

    Add some code in here for more code in here lol

---
+++ app/main.py
@@ -0,0 +1,18 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
+
+class MainApp(App):
+    def build(self):
+        self.icon = "" # add icon in this field
+        self.operators = ["/", "*", "+", "-"] # adding all the operatotrs for this
+        self.last_was_operator = None
+        self.last_button = None
+        
+        main_layout = BoxLayout(orientation = "vertical")
+        self.solution = TextInput(background_color = "black", foreground_color = "white")
+        
+if __name__ == "__main__":
+    app = MainApp()
+    app.run()

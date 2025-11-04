f0f71d2f0cc926b3d8a407990c39eefd9149bd32
Wazingwa Mugala <wazingwamugala90@gmail.com>
2025-11-04 07:33:04 +0200

    Add some more code

--- app/main.py
+++ app/main.py
@@ -13,6 +13,14 @@ class MainApp(App):
         main_layout = BoxLayout(orientation = "vertical")
         self.solution = TextInput(background_color = "black", foreground_color = "white")
         
+        main_layout.add_widget(self.solution)
+        buttons = [
+            ["7", "8", "9", "/"],
+            ["4", "5", "6", "*"],
+            ["1", "2", "3", "-"],
+            [".", "0", "C", "+"],
+        ]
+        
 if __name__ == "__main__":
     app = MainApp()
     app.run()
